9bc3bc4e37f1af8fca0308c63e6a44499ee0dde4
Sediarra <37988823+Sediarra@users.noreply.github.com>
2018-05-02 08:17:41 -0500

    Add files via upload

---
+++ src/Galaxy.java
@@ -0,0 +1,19 @@
+package stories;
+
+import java.util.ArrayList;
+
+import character.Party;
+
+public class Galaxy {
+	private static String enemy1 = "Alien Leader";
+	private static String enemy2 = "Alien Lackey 1";
+	private static String enemy3 = "Alien Lackey 2";
+	
+	public static void startGalaxy(Party party){
+		System.out.println("Galaxy StoryMode Chosen");
+		System.out.println(" "); //create story
+		BattleMode.StartBattle(party, enemy1, enemy2, enemy3);
+		System.out.println("Game Complete");
+
+	}
+}
